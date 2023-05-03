3ac355f3e30fdf272ca49107adb9aa1b59d04ce0
masudra <masudrana6278312@gmail.com>
2023-05-03 20:54:45 +0600

    json data loded  useing useEffect

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -4,21 +4,21 @@ import Login from '../../Login/Login';
 import Baner from '../../Baner/Baner';
 
 const Home = () => {
-    // const [recipe ,setRecipi]=useState([])
+    const [recipe ,setRecipi]=useState([])
 
 
-    // useEffect(()=>{
-    //     fetch('http://localhost:5000/recipi')
-    //     .then(res => res.json())
-    //     .then(data =>setRecipi(data))
-    // },[])
+    useEffect(()=>{
+        fetch('http://localhost:5000/recipi')
+        .then(res => res.json())
+        .then(data =>setRecipi(data))
+    },[])
     return (
         <div>
             <Baner></Baner>
-        {/* {
+        {
             recipe.map(reci =><h1 key={reci.id}>{reci.name}</h1>)
         }
-     */}
+    
             
         </div>
     );
